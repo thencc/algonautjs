e8d20970f955761c04d188634321edd56dd4e812
spencercap <cappiello.spencer@gmail.com>
2022-03-24 17:16:11 +0000

    global + Buffer fix for wallet-connect

--- esbuild.esm.js
+++ esbuild.esm.js
@@ -1,6 +1,10 @@
 // import { build } from 'esbuild'; // for type:"module" pkgs
 const { build } = require('esbuild');
 
+// for wallet-connect shim
+const plugin = require('node-stdlib-browser/helpers/esbuild/plugin');
+const stdLibBrowser = require('node-stdlib-browser');
+
 build({
 	entryPoints: ['src/index.ts'],
 	outdir: 'dist',
@@ -15,5 +19,14 @@ build({
 	format: 'esm', // esm works in node+browser, but cjs is better for node
 	splitting: true, // only for esm
 	outExtension: { '.js': '.mjs' }, // .js -> .mjs (change package.json main + modules entry IF doing this)
+
+	// wallet-connect requires us to shim global -> window and globally avail instance of Buffer
+	inject: [require.resolve('node-stdlib-browser/helpers/esbuild/shim')],
+	define: {
+		global: 'global',
+		process: 'process',
+		Buffer: 'Buffer'
+	},
+	plugins: [plugin(stdLibBrowser)]
 })
 	.catch(() => process.exit(1));
