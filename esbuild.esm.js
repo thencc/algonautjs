f78be3aa3a46980c685e0984ac26d5238b812971
spencercap <cappiello.spencer@gmail.com>
2022-07-18 14:38:06 +0100

    fixed package.json exports block

--- esbuild.esm.js
+++ esbuild.esm.js
@@ -1,6 +1,12 @@
 // import { build } from 'esbuild'; // for type:"module" pkgs
 const { build } = require('esbuild');
 
+// build tips (iife, esm, cjs)
+// https://medium.com/geekculture/build-a-library-with-esbuild-23235712f3c
+
+// about package json 'exports' block
+// https://medium.com/swlh/npm-new-package-json-exports-field-1a7d1f489ccf
+
 build({
 	entryPoints: ['src/index.ts'],
 	outdir: 'dist',
