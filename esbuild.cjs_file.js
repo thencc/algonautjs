d8fc06b2b9d72456af69a80cf14d6e4e5c20332c
Aubrey Anderson <misteranderson@gmail.com>
2022-08-09 16:49:34 -0700

    adventures in cjs

---
+++ esbuild.cjs_file.js
@@ -0,0 +1,37 @@
+// import { build } from 'esbuild'; // for type:"module" pkgs
+const { build } = require('esbuild');
+
+build({
+	entryPoints: ['src/index.ts'],
+	outdir: 'dist',
+	bundle: true,
+	sourcemap: true,
+	minify: true,
+	treeShaking: false,
+
+	// target: ['','node14'], // change for browser/node?
+	// assetNames: 'assets/[name]',
+	// loader: { '.mp3': 'file' },
+	// target: [
+	// 	'es2020',
+	// 	'chrome58',
+	// 	'firefox57',
+	// 	'safari11',
+	// 	'edge16',
+	// 	'node12',
+	// ],
+	// tsconfig: 'custom-tsconfig.json', // tsc path
+	// watch: false, // continuous builds (fast)
+	// define: { DEBUG: 'true' }, // DEFINE replaces global identifiers with constant expressions
+	// drop: ['console'], // things to remove during build
+	// inject: ['./file-with-export.js'] // https://esbuild.github.io/api/#inject
+	// conditions: [] // route import behavior via package.json https://esbuild.github.io/api/#how-conditions-work
+
+	// NODE build
+	// outbase: './cjs',
+	// outdir: './lib' ,
+	platform: 'node',
+	outExtension: { '.js': '.cjs' }
+
+})
+	.catch(() => process.exit(1));
